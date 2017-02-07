ddebebd99f2559898313274b5433862a19cad2e7
Mike Dawson <mike@ustadmobile.com>
2017-02-07 23:24:21 +0400

    Finalize refactoring of testing into core module

---
+++ nanolrs-android/src/androidTest/java/com/ustadmobile/nanolrs/android/model/TestXapiUserAndroid.java
@@ -0,0 +1,31 @@
+package com.ustadmobile.nanolrs.android.model;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.ustadmobile.nanolrs.android.persistence.PersistenceManagerFactoryAndroid;
+import com.ustadmobile.nanolrs.core.manager.XapiUserManager;
+import com.ustadmobile.nanolrs.core.model.TestXapiUser;
+import com.ustadmobile.nanolrs.core.model.XapiUser;
+import com.ustadmobile.nanolrs.core.persistence.PersistenceManager;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by mike on 9/27/16.
+ */
+public class TestXapiUserAndroid extends TestXapiUser{
+
+
+
+
+
+}
