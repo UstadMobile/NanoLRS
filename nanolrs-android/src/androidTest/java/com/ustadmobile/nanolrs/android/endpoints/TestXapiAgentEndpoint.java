046f6e38fef3ee489419229054e986e662a5effe
Mike Dawson <mike@ustadmobile.com>
2017-04-04 18:42:59 +0400

    Fix build process after android gradle update
    Android Gradle update broke system of using unit test compile dependencies with a configuration
    
    We have tests that require an underlying implementation to run but are valid across all platforms. Those tests are now located in their own project, and that project is used as a testCompile dependency for the platform itself (e.g. android)

--- nanolrs-android/src/androidTest/java/com/ustadmobile/nanolrs/android/endpoints/TestXapiAgentEndpoint.java
+++ nanolrs-android/src/androidTest/java/com/ustadmobile/nanolrs/android/endpoints/TestXapiAgentEndpoint.java
@@ -1,21 +1,6 @@
 package com.ustadmobile.nanolrs.android.endpoints;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-
-import com.ustadmobile.nanolrs.android.persistence.PersistenceManagerFactoryAndroid;
 import com.ustadmobile.nanolrs.core.endpoints.TestXapiAgentEndpointCore;
-import com.ustadmobile.nanolrs.core.endpoints.XapiAgentEndpoint;
-import com.ustadmobile.nanolrs.core.manager.XapiAgentManager;
-import com.ustadmobile.nanolrs.core.model.XapiAgent;
-import com.ustadmobile.nanolrs.core.persistence.PersistenceManager;
-
-import org.json.JSONObject;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.List;
 
 /**
  * Created by mike on 10/6/16.
