409ae6b3de8d727bf42794b3b00b1f834ddf89c8
Varuna Singh <varuna.singh@gmail.com>
2017-05-06 16:25:05 +0400

    Updated test env of nanolrs-jdbc so sharedse-tests. Bumped version

---
+++ nanolrs-jdbc/src/test/java/com/ustadmobile/port/sharedse/persistence/util/NanoLrsPlatformTestUtil.java
@@ -0,0 +1,11 @@
+package com.ustadmobile.port.sharedse.persistence.util;
+
+/**
+ * Created by varuna on 5/6/2017.
+ */
+
+public class NanoLrsPlatformTestUtil {
+    public static Object getContext() {
+        return com.ustadmobile.nanolrs.core.util.NanoLrsPlatformTestUtil.getContext();
+    }
+}
