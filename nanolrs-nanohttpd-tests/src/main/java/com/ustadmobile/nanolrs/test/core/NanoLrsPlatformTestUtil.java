d453cd63ef838d8f41ba8f7a0ddacd7ce84bd976
Mike Dawson <mike@ustadmobile.com>
2017-05-09 17:29:04 +0400

    Introduce http test project with coverage

---
+++ nanolrs-nanohttpd-tests/src/main/java/com/ustadmobile/nanolrs/test/core/NanoLrsPlatformTestUtil.java
@@ -0,0 +1,12 @@
+package com.ustadmobile.nanolrs.test.core;
+
+/**
+ * Created by mike on 5/9/17.
+ */
+
+public class NanoLrsPlatformTestUtil {
+
+    public static Object getContext() {
+        throw new RuntimeException("Use an implementation - this is just a stub");
+    }
+}
