fe5e9584b8e5bcc097bba82a556384022356d88d
Varuna Singh <varuna.singh@gmail.com>
2017-05-29 22:55:15 +0400

    Changed dates to long

--- nanolrs-core/src/main/java/com/ustadmobile/nanolrs/core/model/NanoLrsModel.java
+++ nanolrs-core/src/main/java/com/ustadmobile/nanolrs/core/model/NanoLrsModel.java
@@ -1,6 +1,6 @@
 package com.ustadmobile.nanolrs.core.model;
 
-import com.j256.ormlite.field.types.DateTimeType;
+//import com.j256.ormlite.field.types.DateTimeType;
 
 /**
  * Created by mike on 4/30/17.
@@ -18,14 +18,14 @@ public interface NanoLrsModel {
     String getMasterSequence();
     void setMasterSequence(String masterSequence);
 
-    DateTimeType getDateCreated();
-    void setDateCreated(DateTimeType dateCreated);
+    long getDateCreated();
+    void setDateCreated(long dateCreated);
 
-    DateTimeType getDateStored();
-    void setDateStored(DateTimeType dateStored);
+    long getDateStored();
+    void setDateStored(long dateStored);
 
-    DateTimeType getDateModifiedAtMaster();
-    void setDateModifiedAtMaster(DateTimeType dateModifiedAtMaster);
+    long getDateModifiedAtMaster();
+    void setDateModifiedAtMaster(long dateModifiedAtMaster);
 
 
 
