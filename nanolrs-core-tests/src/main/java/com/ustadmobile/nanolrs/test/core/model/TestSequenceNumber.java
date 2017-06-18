b6cd90ae37a676e64006a9ad1447217f8e6cd919
Varuna Singh <varuna.singh@gmail.com>
2017-06-18 23:49:37 +0400

    Tests ok with m2m / o2m relationships

--- nanolrs-core-tests/src/main/java/com/ustadmobile/nanolrs/test/core/model/TestSequenceNumber.java
+++ nanolrs-core-tests/src/main/java/com/ustadmobile/nanolrs/test/core/model/TestSequenceNumber.java
@@ -1,5 +1,6 @@
 package com.ustadmobile.nanolrs.test.core.model;
 
+
 import com.ustadmobile.nanolrs.core.manager.RelationshipTest2StudentManager;
 import com.ustadmobile.nanolrs.core.manager.RelationshipTest2TeacherManager;
 import com.ustadmobile.nanolrs.core.manager.RelationshipTestManager;
@@ -14,7 +15,9 @@ import com.ustadmobile.nanolrs.test.core.NanoLrsPlatformTestUtil;
 import org.junit.Assert;
 import org.junit.Test;
 
-
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.UUID;
 
@@ -36,15 +39,15 @@ public class TestSequenceNumber {
         userManager.persist(context, newUser);
 
         XapiUser anotherNewUser = userManager.createSync(context, UUID.randomUUID().toString());
-        anotherNewUser.setUsername("anothertestuser");
+        anotherNewUser.setUsername("testuser2");
         userManager.persist(context, anotherNewUser);
 
         XapiUser newUser3 = userManager.createSync(context, UUID.randomUUID().toString());
-        newUser3.setUsername("anothertestuser");
+        newUser3.setUsername("testuser3");
         userManager.persist(context, newUser3);
 
         XapiUser newUser4 = userManager.createSync(context, UUID.randomUUID().toString());
-        newUser3.setUsername("anothertestuser");
+        newUser4.setUsername("testuser4");
         userManager.persist(context, newUser4);
 
         //create a relationship
@@ -56,10 +59,9 @@ public class TestSequenceNumber {
 
         newRelationshipTest.setName("New Relationship");
 
-        //Do we need to persist before m2m ? 
+        //Do we need to persist before m2m ?
         //Update: Nope, we do not need to persist before
         //relationshipTestManager.persist(context, newRelationshipTest);
-
 
         //create an relationshipTest m2m with users (assign students)
         RelationshipTest2StudentManager relationshipTest2StudentManager =
@@ -68,13 +70,21 @@ public class TestSequenceNumber {
                 (RelationshipTest2Student) relationshipTest2StudentManager.makeNew();
         newRelationshipTest2Student.setUuid(UUID.randomUUID().toString());
         newRelationshipTest2Student.setRelationshipTest(newRelationshipTest);
-        newRelationshipTest2Student.setStudent(anotherNewUser);
+        newRelationshipTest2Student.setStudent(newUser);
         relationshipTest2StudentManager.persist(context, newRelationshipTest2Student );
 
+        //another student..
+        RelationshipTest2StudentManager relationshipTest2StudentManager2 =
+                PersistenceManager.getInstance().getManager(RelationshipTest2StudentManager.class);
+        RelationshipTest2Student newRelationshipTest2Student2 =
+                (RelationshipTest2Student) relationshipTest2StudentManager2.makeNew();
+        newRelationshipTest2Student2.setUuid(UUID.randomUUID().toString());
+        newRelationshipTest2Student2.setRelationshipTest(newRelationshipTest);
+        newRelationshipTest2Student2.setStudent(anotherNewUser);
+        relationshipTest2StudentManager2.persist(context, newRelationshipTest2Student2 );
 
 
-        //newRelationshipTest.setTestStudents();
-
+        //Creating a relationshipTest m2m with users(assign teachers)
         RelationshipTest2TeacherManager relationshipTest2TeacherManager =
                 PersistenceManager.getInstance().getManager(RelationshipTest2TeacherManager.class);
         RelationshipTest2Teacher newRelationshipTest2Teacher =
@@ -84,20 +94,69 @@ public class TestSequenceNumber {
         newRelationshipTest2Teacher.setTeacher(newUser3);
         relationshipTest2TeacherManager.persist(context, newRelationshipTest2Teacher);
 
+        //another teacher..
+        RelationshipTest2TeacherManager relationshipTest2TeacherManager2 =
+                PersistenceManager.getInstance().getManager(RelationshipTest2TeacherManager.class);
+        RelationshipTest2Teacher newRelationshipTest2Teacher2 =
+                (RelationshipTest2Teacher) relationshipTest2TeacherManager2.makeNew();
+        newRelationshipTest2Teacher2.setUuid(UUID.randomUUID().toString());
+        newRelationshipTest2Teacher2.setRelationshipTest(newRelationshipTest);
+        newRelationshipTest2Teacher2.setTeacher(newUser4);
+        relationshipTest2TeacherManager2.persist(context, newRelationshipTest2Teacher2);
+
+
+        /* //UPDATE: Cannot set Foreign Collections like so..
+        //newRelationshipTest.setTestStudents();
+        //newRelationshipTest.setTestStudents(Collection<? extends RelationshipTest2Student> testStudents);
+
+        //ForeignCollection<RelationshipTest2Student> testStudents;
+        //testStudents = new ForeignCollection<RelationshipTest2Student>;
+        //^ doesnt work..
+
+        //Collection testStudents = null;
+        //testStudents = new Collection<RelationshipTest2Student>();
+        testStudents = new HashSet(); //works but cannot cast to foreign. We cannot do this.
+        testStudents.add(newRelationshipTest2Student);
+        testStudents.add(newRelationshipTest2Student2);
+
+        newRelationshipTest.setTestStudents(testStudents);
+        */
+        /* UPDATE: Cannot set Foreign Collections like so..
         //newRelationshipTest.setTestTeachers();
-
-
+        */
 
         relationshipTestManager.persist(context, newRelationshipTest);
-
         long localSeqNumber = newRelationshipTest.getLocalSequence();
         Assert.assertNotNull(localSeqNumber);
 
         newRelationshipTest.setNotes("This is an update to the relationship..");
         relationshipTestManager.persist(context, newRelationshipTest);
-
         long newLocalSeqNumber = newRelationshipTest.getLocalSequence();
-
         Assert.assertEquals(localSeqNumber +1, newLocalSeqNumber);
+
+        RelationshipTest theNewRelationshipTest = (RelationshipTest)
+                relationshipTestManager.findByPrimaryKey(context, newRelationshipTest.getUuid());
+        Collection<? extends RelationshipTest2Student> rtStudents =
+                theNewRelationshipTest.getTestStudents();
+        Collection<? extends RelationshipTest2Teacher> rtTeachers =
+                theNewRelationshipTest.getTestTeachers();
+
+        Assert.assertEquals(rtStudents.isEmpty(), false);
+        Assert.assertEquals(rtTeachers.isEmpty(), false);
+        Assert.assertEquals(rtStudents.size(), 2);
+        Assert.assertEquals(rtTeachers.size(), 2);
+
+        if(rtStudents.iterator().hasNext()){
+            XapiUser student = rtStudents.iterator().next().getStudent();
+            String studentUsername = student.getUsername();
+            Assert.assertEquals(studentUsername, "testuser");
+        }
+        if(rtTeachers.iterator().hasNext()){
+            XapiUser teacher = rtTeachers.iterator().next().getTeacher();
+            String teacherUsername = teacher.getUsername();
+            Assert.assertEquals(teacherUsername, "testuser4");
+        }
+
+
     }
 }
